2a0f193323d741106d43118cc03fed9d5dc80138
Alexander Borisenko <wealthybora@gmail.com>
2016-04-17 19:10:34 +0300

    comment REST

---
+++ api/controllers/CommentController.js
@@ -0,0 +1,13 @@
+/**
+ * CommentController
+ *
+ * @description :: Server-side logic for managing comments
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	/*_config: {
+    restPrefix: '/api/v1',
+  }*/
+};
+
---
+++ api/models/Comment.js
@@ -0,0 +1,20 @@
+/**
+ * Comment.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+    text: {
+      type: 'string',
+      required: true
+    }
+  }
+};
+
