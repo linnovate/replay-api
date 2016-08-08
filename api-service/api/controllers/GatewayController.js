b07aec61a929de93bb8803c9a0fd5225d4e20098
Aviad Morshet <aviad10m@gmail.com>
2016-08-08 16:16:50 +0300

    Fix GatewayController so it will route more methods other than GET. Fixed linnovate/replay-api#135

--- api-service/api/controllers/GatewayController.js
+++ api-service/api/controllers/GatewayController.js
@@ -4,24 +4,33 @@
  * @description :: Server-side logic for managing routings
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
- // request param as set in route.js config
-const REQUEST_METHOD = 'method';
+// request param as set in route.js config
+
 const request = require('request');
 
 module.exports = {
-	gate: function(req, res, next) {
-		var baseUrl;
-		var method = req.param(REQUEST_METHOD);
-		var params = req.originalUrl.substring(req.originalUrl.indexOf(method));
-		var serviceConf = sails.config.settings.services[method];
-		if (serviceConf !== undefined) {
-			baseUrl = serviceConf.url + ':' + serviceConf.port;
-		} else {
-			return res.badRequest('unknown service');
-		}
-    request(baseUrl + '/' + params, function (error, response, body) {
-      if (error) res.status(500, error);
-      res.ok(body);
-    });
-  }
+    gate: function(req, res, next) {
+        var baseUrl;
+        var service = req.param('service');
+        var params = req.originalUrl.substring(req.originalUrl.indexOf(service));
+        var serviceConf = sails.config.settings.services[service];
+        if (serviceConf !== undefined) {
+            baseUrl = serviceConf.url + ':' + serviceConf.port;
+        } else {
+            return res.notFound();
+        }
+
+        request({
+            method: req.method,
+            url: baseUrl + '/' + params,
+            json:true,
+            body: req.body
+        }, function(err, response, body) {
+            if (err) {
+                return next(err);
+            }
+            res.ok(body);
+        });
+
+    }
 };
