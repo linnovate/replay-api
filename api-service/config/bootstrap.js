41c0a782b4852971f6bb1cc76fe0b4ba38d0d1a9
Aviad Morshet <aviad10m@gmail.com>
2016-08-24 09:57:42 +0300

    Removed unused mongo connect

--- api-service/config/bootstrap.js
+++ api-service/config/bootstrap.js
@@ -15,10 +15,5 @@ module.exports.bootstrap = function(cb) {
 
     // It's very important to trigger this callback method when you are finished
     // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-    connectMongo(process.env.MONGO_HOST, process.env.MONGO_PORT, process.env.MONGO_DATABASE)
-        .then(cb)
-        .catch(function(err) {
-            console.log('Some error occured in bootstrap.');
-            console.log(err);
-        });
+   cb();
 };
