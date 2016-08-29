08f2921746e15f60c4cf3cda222d23f1d668a9dd
motyros <motyros@gmail.com>
2016-08-29 13:21:26 +0300

    restore authorizationXml files

---
+++ api-service/api/controllers/AuthorizationXmlController.js
@@ -0,0 +1,32 @@
+/**
+ * AuthorizationXmlController
+ *
+ * @description :: Server-side logic for serving authorization xmls files
+ */
+
+var fileSystem = require('fs');
+
+module.exports = {
+
+	/**
+	 * `AuthorizationXmlController.serve()`
+	 */
+	serve: function(req, res) {
+		var id = req.params.id;
+		var fileName = 'sys';
+		if (id) {
+			fileName += id;
+		}
+		var target = './assets/authorizationXml/' + fileName + '.xml';
+		fileSystem.exists(target, function(exists) {
+			if (!exists) {
+				return res.notFound('The requested file (' + fileName + ') does not exist.');
+			}
+
+			res.writeHead(200, { 'Content-Type': 'text/xml' });
+			var readStream = fileSystem.createReadStream(target);
+			// We replaced all the event handlers with a simple call to readStream.pipe()
+			readStream.pipe(res);
+		});
+	}
+};
