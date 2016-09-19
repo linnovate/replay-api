a47283efaef346953e8cd36f07e03606dc9540ba
Aviad Morshet <aviad10m@gmail.com>
2016-09-19 12:44:53 +0300

    Added swagger to media-url-provider

--- query-service/config/env/development.js
+++ query-service/config/env/development.js
@@ -24,8 +24,5 @@ module.exports = {
 		host: process.env.MONGO_HOST || 'localhost',
 		port: process.env.MONGO_PORT || 27017,
 		database: process.env.MONGO_DATABASE || 'replay_dev'
-	},
-	swagger: {
-		pkg: require('../.././package')
 	}
 };
