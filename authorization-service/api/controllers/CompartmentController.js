6096ba40e5825cf4f03477dc2e3e70540397d543
Aviad Morshet <aviad10m@gmail.com>
2016-10-25 18:16:25 +0300

    Query videos with compartment is now finished

--- authorization-service/api/controllers/CompartmentController.js
+++ authorization-service/api/controllers/CompartmentController.js
@@ -4,134 +4,24 @@
  * @description :: Server-side logic for serving authorization xmls files
  */
 
-var fs = require('fs');
-var xml2js = require('xml2js');
-var Promise = require('bluebird');
-
-var parser = new xml2js.Parser();
-
 module.exports = {
 
-	/**
-	 * `AuthorizationXmlController.serve()`
-	 */
-	getCompartment: function(req, res) {
-		var id = req.params.id;
-		return getCompartment(id)
-			.then(parseXml)
-			.then(function(result) {
+	find: function (req, res, next) {
+		var id = req.query.id;
+		return validateRequest(req.query)
+			.then(() => CompartmentService.getCompartmentPermissions(id))
+			.then(function (result) {
 				res.send(result);
 			})
-			.catch(function(err) {
-				res.notFound(err);
+			.catch(function (err) {
+				res.serverError(err);
 			});
-	},
-	generateCompartmentCondition: function(req, res) {
-		var id = req.params.id;
-		return getCompartment(id)
-			.then(parseXml)
-			.then(buildQuery)
-			.then(function(result) {
-				res.send(result);
-			})
-			.catch(function(err) {
-				res.notFound(err);
-			});
-	},
-	permission: function(req, res) {
-		var id = req.params.id;
-		return getCompartment(id)
-			.then(parseXml)
-			.then(function(result) {
-				res.send(result);
-			})
-			.catch(function(err) {
-				res.notFound(err);
-			});
-	},
+	}
 };
 
-function getCompartment(id) {
-	var fileName = 'sys';
-	if (id) {
-		fileName += id;
+function validateRequest(params) {
+	if (!params.id) {
+		return Promise.reject(new Error('Missing id in query parameters'));
 	}
-	return new Promise(function(resolve, reject) {
-		var target = './assets/authorizationXml/' + fileName + '.xml';
-		fs.readFile(target, function(err, data) {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(data);
-			}
-		});
-	});
+	return Promise.resolve();
 }
-
-function buildQuery(permissions) {
-	return new Promise(function(resolve, reject) {
-		try {
-			var query = { $or: [] };
-			for (var i = 0; i < permissions.length; i++) {
-				query.$or.push({
-					destination: permissions[i].id[0]
-				});
-				if (i === permissions.length - 1) {
-					console.log('query: ' + JSON.stringify(query));
-					resolve(query);
-					break;
-				}
-			}
-		} catch (err) {
-			reject(err);
-		}
-	});
-}
-
-function parseXml(data) {
-	return new Promise(function(resolve, reject) {
-		parser.parseString(data, function(err, result) {
-			if (result === undefined || err) {
-				reject(err);
-			} else {
-				var permissions = result.permissions.allow[0].userPermission;
-				resolve(permissions);
-			}
-		});
-	});
-}
-
-/*function getUserPermissions(userCode) {
-	var PromiseRequest = Promise.method(function(options) {
-		return new Promise(function(resolve, reject) {
-			var request = http.request(options, function(response) {
-				// Bundle the result
-				response.setEncoding('utf8');
-				var responseString = '';
-
-				response.on('data', function(data) {
-					responseString += data;
-				});
-
-				response.on('end', function() {
-					resolve(responseString);
-				});
-			});
-
-			// Handle errors
-			request.on('error', function(error) {
-				console.log('Problem with request:', error.message);
-				reject(error);
-			});
-
-			request.end();
-		});
-	});
-
-	return PromiseRequest({
-		host: process.env.COMPARTMENT_HOST || 'localhost', //
-		port: process.env.COMPARTMENT_PORT || 1340, //
-		path: '/compartment/getCompartment',
-		method: 'GET'
-	});
-}*/
--- query-service/api/services/PermissionsService.js
+++ query-service/api/services/PermissionsService.js
@@ -1,9 +1,11 @@
 var requestAsPromise = require('request-promise');
 
 module.exports.findPermissionsByUserId = function(userId) {
+	console.log('Finding permission to user of id:', userId);
 	return UserService.findUserById(userId)
 		.then(user => {
 			if(user) {
+				console.log('Found user:', JSON.stringify(user));
 				return getPermissions(user.providerDetails.id);
 			}
 
@@ -12,10 +14,11 @@ module.exports.findPermissionsByUserId = function(userId) {
 };
 
 function getPermissions(userProviderId) {
-	var authorizationServiceUrl = sails.config.services.authorization_service.host + ':' + sails.config.services.authorization_service.port + '/compartment/permission';
+	var authorizationServiceUrl = sails.config.services.authorization_service.host + ':' + sails.config.services.authorization_service.port + '/compartment';
 	console.log('Requesting permissions from authorization-service in url:', authorizationServiceUrl);
 	var options = {
 		url: authorizationServiceUrl,
+		qs: { id: userProviderId },
 		json: true
 	};
 	return requestAsPromise(options)
