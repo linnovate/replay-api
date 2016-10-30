3b0f9c12c06838ca62cae3023c0a054be1a0165e
Aviad Morshet <aviad10m@gmail.com>
2016-10-30 16:53:32 +0200

    Create CRUD stubs

---
+++ playlist-service/api/controllers/PlaylistController.js
@@ -0,0 +1,22 @@
+/**
+ * PlaylistController
+ *
+ * @description :: Server-side logic for managing playlists
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	find: function(req, res, next) {
+
+	},
+	create: function(req, res, next) {
+
+	},
+	update: function(req, res, next) {
+
+	},
+	destroy: function(req, res, next) {
+
+	}
+};
+
