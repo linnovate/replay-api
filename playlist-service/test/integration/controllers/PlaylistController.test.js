274ad8175577d49352d1f9fa404eb0a7786ad08f
Aviad Morshet <aviad10m@gmail.com>
2016-10-31 13:09:12 +0200

    Playlist-service: Added test for playlist creation

--- playlist-service/test/integration/controllers/PlaylistController.test.js
+++ playlist-service/test/integration/controllers/PlaylistController.test.js
@@ -1,34 +1,41 @@
 var Playlist = require('replay-schemas/Playlist'),
-	request = require('supertest-as-promised'),
-	Promise = require('bluebird'),
-	authorizationMock = require('replay-test-utils/authorization-mock');
+    request = require('supertest-as-promised'),
+    Promise = require('bluebird'),
+    authorizationMock = require('replay-test-utils/authorization-mock');
 
 describe('PlaylistController', () => {
 
-	describe('#find()', () => {
-		var tagStubsAmount = 3;
-        it(`should return ${tagStubsAmount} playlists`, function (done) {
-            createPlaylists(tagStubsAmount)
+    describe('#find()', () => {
+        var tagStubsAmount = 3;
+        it(`should return ${tagStubsAmount} playlists`, done => {
+            createPlaylistsInMongo(tagStubsAmount)
                 .then(() => getAndExpectPlaylists(tagStubsAmount))
                 .then(done)
                 .catch(done);
         });
 
-        it('should return 0 playlists', function (done) {
+        it('should return 0 playlists', done => {
             getAndExpectPlaylists(0)
                 .then(done)
                 .catch(done);
         })
-	});
+    });
+
+    describe('#create()', () => {
+        it('should create a playlist', done => {
+            var playlist = generatePlaylist();
+            createAndExpectPlaylist(playlist)
+                .then(() => validatePlaylistCreated(playlist))
+                .then(() => done())
+                .catch(done);
+        });
+    });
 });
 
-function createPlaylists(amount) {
-	var playlist = {
-		name: 'test',
-		ownerId: authorizationMock.getUser().id
-	};
+function createPlaylistsInMongo(amount) {
+    var playlist = generatePlaylist();
 
-	var promises = [];
+    var promises = [];
     for (var i = 0; i < amount; i++) {
         playlist.title = playlist.title + i;
         promises.push(Playlist.create(playlist));
@@ -45,4 +52,31 @@ function getAndExpectPlaylists(amount) {
         .then((res) => {
             expect(res.body).to.have.lengthOf(amount);
         });
+}
+
+function generatePlaylist(){
+    return {
+        name: 'test',
+        ownerId: authorizationMock.getUser().id
+    };
+}
+
+function createAndExpectPlaylist(playlist) {
+    return request(sails.hooks.http.app)
+        .post('/playlist')
+        .send(playlist)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200);
+}
+
+function validatePlaylistCreated(playlist) {
+    return Playlist.findOne(playlist)
+        .then(playlist => {
+            if(playlist) {
+                return Promise.resolve();
+            }
+            
+            return Promise.reject(new Error('Playlist was not found in mongo.'));
+        })
 }
