462d6053d538833adcaf599c427290580e09da73
Aviad Morshet <aviad10m@gmail.com>
2016-11-01 18:43:42 +0200

    User-service: Implemented remove favorite mission test, does not pass yet

--- user-service/test/integration/controllers/UserController.test.js
+++ user-service/test/integration/controllers/UserController.test.js
@@ -21,11 +21,32 @@ describe('UserController', () => {
 
     describe('#updateFavorite()', () => {
         it('should add favorite mission to user', done => {
-            done();
+            var userId = authorizationMock.getUser().id;
+            var missionId;
+
+            createMissionInMongo()
+                .then(mission => {
+                    missionId = mission.id;
+                    return addFavoriteMissionToUser(userId, missionId);
+                })
+                .then(() => validateFavoriteAddedToUser(userId, missionId))
+                .then(() => done())
+                .catch(done);
         });
 
         it('should remove favorite mission from user', done => {
-            done();
+            var userId = authorizationMock.getUser().id;
+            var missionId;
+
+            createMissionInMongo()
+                .then(mission => {
+                    missionId = mission.id;
+                    return addFavoriteMissionToUser(userId, missionId);
+                })
+                .then(() => removeFavoriteMissionFromUser(userId, missionId))
+                .then(() => validateFavoriteRemovedFromUser(userId, missionId))
+                .then(() => done())
+                .catch(done);
         });
     });
 
@@ -42,6 +63,24 @@ describe('UserController', () => {
     });
 
     describe('#updateFavorite() bad input tests', () => {
+        afterEach(done => {
+            var userId = authorizationMock.getUser().id;
+            User.findById(userId)
+                .then(user => {
+                    user.favorites = [];
+                    return user.save();
+                })
+                .then(() => {
+                    return User.findById(userId)
+                        .then(user => {
+                            console.log('User favorites:', user.favorites);
+                            return Promise.resolve();
+                        })
+                })
+                .then(() => done())
+                .catch(done);
+        });
+
         it('should reject due to bad user id', done => {
             var userId = 'someId';
             var missionId = new mongoose.Types.ObjectId();
@@ -87,4 +126,57 @@ function updateUserFavoriteAndExpectBadRequest(userId, missionId, done) {
         .put(userUrl)
         .set('Accept', 'application/json')
         .expect(400, done);
+}
+
+function createMissionInMongo() {
+     var mission = {
+        missionName: 'test',
+        sourceId: '123',
+        startTime: Date.now(),
+        endTime: Date.now(),
+        destination: 'System 1'
+    };
+
+    return Mission.create(mission);
+}
+
+function addFavoriteMissionToUser(userId, missionId) {
+    var userUrl = util.format(userUpdateFavoriteFormat, userId, missionId);
+    
+    return request(sails.hooks.http.app)
+        .put(userUrl)
+        .set('Accept', 'application/json')
+        .expect(200);
+}
+
+function validateFavoriteAddedToUser(userId, missionId) {
+    return User.findById(userId)
+        .then(user => {
+            if(user && user.favorites[0].toString() === missionId) {
+                return Promise.resolve();
+            }
+
+            return Promise.reject(new Error('Favorite mission was not added to user.'));
+        });
+}
+
+function removeFavoriteMissionFromUser(userId, missionId) {
+    var userUrl = util.format(userUpdateFavoriteFormat, userId, missionId);
+    console.log('delete url:', userUrl);
+    return request(sails.hooks.http.app)
+        .delete(userUrl)
+        .set('Accept', 'application/json')
+        .expect(200);
+}
+
+function validateFavoriteRemovedFromUser(userId, missionId) {
+    return User.findById(userId)
+        .then(user => {
+            console.log('favorites:', user.favorites);
+            if(user && user.favorites.length === 0) {
+                return Promise.resolve();
+            }
+
+            return Promise.reject(new Error('Favorite mission was not removed from user.'));
+        });
 }
