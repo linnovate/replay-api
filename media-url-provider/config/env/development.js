ac870c26bc732a356a26343ee33d9fd93b2f13ba
Aviad Morshet <aviad10m@gmail.com>
2016-11-08 13:27:08 +0200

    Added mongo auth to media-url-provider

--- media-url-provider/config/env/development.js
+++ media-url-provider/config/env/development.js
@@ -34,7 +34,9 @@ module.exports = {
 	mongo: {
 		host: process.env.MONGO_HOST || 'localhost',
 		port: process.env.MONGO_PORT || 27017,
-		database: process.env.MONGO_DATABASE || 'replay_dev'
+		database: process.env.MONGO_DATABASE || 'replay_dev',
+		username: process.env.MONGO_USERNAME,
+		password: process.env.MONGO_PASSWORD
 	}
 };
 
--- media-url-provider/config/env/test.js
+++ media-url-provider/config/env/test.js
@@ -38,6 +38,8 @@ module.exports = {
 	mongo: {
 		host: process.env.MONGO_HOST || '127.0.0.1',
 		port: process.env.MONGO_PORT || 27017,
-		database: process.env.MONGO_DATABASE || 'replay_test'
+		database: process.env.MONGO_DATABASE || 'replay_test',
+		username: process.env.MONGO_USERNAME,
+		password: process.env.MONGO_PASSWORD
 	}
 };
