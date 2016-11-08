e1bf192256b9df08f3212ebd9d9a2a625f8ee6d7
Aviad Morshet <aviad10m@gmail.com>
2016-11-08 13:42:19 +0200

    Added mongo auth to query service

--- query-service/config/env/testing.js
+++ query-service/config/env/testing.js
@@ -4,6 +4,8 @@ module.exports = {
 	mongo: {
 		host: process.env.MONGO_HOST || 'localhost',
 		port: process.env.MONGO_PORT || 27017,
-		database: process.env.MONGO_DATABASE || 'replay_test'
+		database: process.env.MONGO_DATABASE || 'replay_test',
+		username: process.env.MONGO_USERNAME,
+		password: process.env.MONGO_PASSWORD
 	}
 }
